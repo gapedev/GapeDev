3d79049658f72d6777299c0f6b7d1ecf0d287717
Gabi Pérez <maquetacion@clicktotravel.es>
2023-03-31 18:21:53 +0200

    feat: info component

---
+++ src/lib/caja-info.svelte
@@ -0,0 +1,39 @@
+<script>
+  const titles = [
+    "Nombre",
+    "Profesión",
+    "Experiencia",
+    "Email",
+    "Linkedin",
+    "Web",
+    "Teléfono",
+  ];
+  const info = [
+    "Gabi Pérez Licerán",
+    "Front-End Developer",
+    "5 Años",
+    "gapedev@gmail.com",
+    "https://www.linkedin.com/in/gabriel-p%C3%A9rez-licer%C3%A1n-b0826a14b?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BvIfRJmdNRgCguGUgbDJ5dQ%3D%3D",
+    "https://gapedev.netlify.app/",
+    "601 06 87 78",
+  ];
+</script>
+
+<div class="about">
+  {#each titles as title}
+    <span>{title}:</span>
+  {/each}
+</div>
+
+<style lang="scss">
+  .about {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 10px;
+
+    span {
+      color: $white;
+      font-weight: bold;
+    }
+  }
+</style>
---
+++ src/lib/title.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let title;
+</script>
+
+<div class="title">
+  <h2>{title}</h2>
+</div>
+
+<style lang="scss">
+  .title {
+    h2 {
+      color: $white;
+      font-size: 2rem;
+      font-weight: bold;
+      position: relative;
+
+      &:before {
+        content: "";
+        background-color: $secondary;
+        width: 50px;
+        height: 3px;
+        position: absolute;
+        top: 40px;
+        left: 0;
+      }
+    }
+  }
+</style>
