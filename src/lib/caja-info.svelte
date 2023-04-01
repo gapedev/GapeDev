e35d7f626c57c2051b0db6bdfa3b216a1d43b1bf
Gabi Pérez <maquetacion@clicktotravel.es>
2023-04-01 09:54:57 +0200

    feat: info component logic

--- src/lib/caja-info.svelte
+++ src/lib/caja-info.svelte
@@ -1,27 +1,30 @@
 <script>
-  const titles = [
-    "Nombre",
-    "Profesión",
-    "Experiencia",
-    "Email",
-    "Linkedin",
-    "Web",
-    "Teléfono",
-  ];
-  const info = [
-    "Gabi Pérez Licerán",
-    "Front-End Developer",
-    "5 Años",
-    "gapedev@gmail.com",
-    "https://www.linkedin.com/in/gabriel-p%C3%A9rez-licer%C3%A1n-b0826a14b?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BvIfRJmdNRgCguGUgbDJ5dQ%3D%3D",
-    "https://gapedev.netlify.app/",
-    "601 06 87 78",
+  let infos = [
+    {
+      nombre: "Gabi Pérez Licerán",
+      profesion: "Front-End Developer",
+      experiencia: "5 Años",
+      email: "gapedev@gmail.com",
+      linkedin:
+        "https://www.linkedin.com/in/gabriel-p%C3%A9rez-licer%C3%A1n-b0826a14b?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BvIfRJmdNRgCguGUgbDJ5dQ%3D%3D",
+      web: "https://gapedev.netlify.app/",
+      telefono: "601 06 87 78",
+    },
   ];
 </script>
 
 <div class="about">
-  {#each titles as title}
-    <span>{title}:</span>
+  {#each infos as info (info)}
+    <slot
+      name="gabi"
+      nombre={info.nombre}
+      profesion={info.profesion}
+      experiencia={info.experiencia}
+      email={info.email}
+      linkedin={info.linkedin}
+      web={info.web}
+      telefono={info.telefono}
+    />
   {/each}
 </div>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <script>
+  import CajaInfo from "$lib/caja-info.svelte";
 </script>
+
+<CajaInfo>
+  <h3 slot="gabi" let:nombre let:email>
+    {nombre}
+    {email}
+  </h3>
+</CajaInfo>
