28cbb88730416a15a2e8e6bcd6c20e3d15a46321
Gabi Pérez <maquetacion@clicktotravel.es>
2024-01-03 18:58:13 +0100

    feat: navbar styles mobile

---
+++ src/services/navbar.js
@@ -0,0 +1,21 @@
+export const NAVBAR = [
+  {
+    name: 'Home',
+    path: '/'
+  },
+  {
+    name: 'About',
+    path: '/about'
+  },
+  {
+    name: 'Portfolio',
+    path: '/portfolio'
+  },
+  {
+    name: 'Projects',
+    path: '/projects'
+  },
+  {
+    name: 'Contact',
+    path: '/contact'
+  }]
